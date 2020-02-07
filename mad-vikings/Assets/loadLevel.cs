c8cd095b9232060998dd6618716767e0817f4163
sullivan <sullivan.verdan@live.fr>
2020-02-07 06:01:54 +0100

    [ADD] two levels

---
+++ mad-vikings/Assets/loadLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    public string levelname = "enter level name";
+    public string levelToRestart = "enter level name";
+
+    public void loadTheLevel() {
+        SceneManager.LoadScene(levelname);
+    }
+
+    public void reloadTheLevel() {
+        SceneManager.LoadScene(levelToRestart);
+    }
+}
---
+++ mad-vikings/Assets/triggerPause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerPause : MonoBehaviour
+{
+    bool showGui = false;
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start() {
+        canvas = GameObject.Find("CanvasPause");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            showGui = !showGui;
+        }
+
+        if (showGui) {
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            canvas.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void returnInTheGame() {
+        showGui = !showGui;
+    }
+}
