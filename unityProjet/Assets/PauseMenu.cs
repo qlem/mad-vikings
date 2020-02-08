42d40e9949a964f3e15c25df5c133f7349c4860c
borisclarence <borisclarence27@gmail.com>
2020-02-08 12:52:55 +0100

    add menu

---
+++ unityProjet/Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) 
+            {
+                Resume();
+            }  else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.log("Loading Menu...");
+        //SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.log("Quitting game...");
+        //Application.Quit();
+    }
+}
